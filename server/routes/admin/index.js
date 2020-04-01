749796f336943cf484f718f3aa66b927329dd8b5
yukiice <1076702326@qq.com>
2020-04-01 23:09:52 +0800

    通用crud接口开发完成

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,38 +1,52 @@
 module.exports = app => {
     const express = require('express')
-    const router = express.Router()
-    const Category = require('../../models/Category')
+    const router = express.Router({
+        mergeParams: true
+    })
 
-    router.post('/categories', async (req, res) => {
-        const model = await Category.create(req.body)
+
+    router.post('/', async (req, res) => {
+        const model = await req.Model.create(req.body)
         res.send(model)
     })
 
     //分类列表接口
-    router.get('/categories', async (req, res) => {
-        const items = await Category.find().populate('parent').limit(10)
+    router.get('/', async (req, res) => {
+        // 防止接口过多的查询 加入判断条件
+        const queryOptions = {}
+        // 需要满足来自接口Category 才能去查询parent
+        if (req.Model.modelName === 'Category') {
+            queryOptions.populate = 'parent'
+        }
+        const items = await req.Model.find().setOptions(queryOptions).limit(10)
         res.send(items)
     })
 
     // 分类详情页接口
-    router.get('/categories/:id', async (req, res) => {
-        const model = await Category.findById(req.params.id)
+    router.get('/:id', async (req, res) => {
+        const model = await req.Model.findById(req.params.id)
         res.send(model)
     })
 
     // 编辑保存接口
-    router.put('/categories/:id', async (req, res) => {
-        const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    router.put('/:id', async (req, res) => {
+        const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
 
     // 删除分类接口
-    router.delete('/categories/:id', async (req, res) => {
-        await Category.findByIdAndDelete(req.params.id, req.body)
+    router.delete('/:id', async (req, res) => {
+        await req.Model.findByIdAndDelete(req.params.id, req.body)
         res.send({
             success: true
         })
     })
 
-    app.use('/admin/api', router)
+    app.use('/admin/api/rest/:resource',
+        // 采用中间件处理
+        async (req, res, next) => {
+            const modelName = require('inflection').classify(req.params.resource)
+            req.Model = require(`../../models/${modelName}`)
+            next()
+        }, router)
 }
